cfb6292da5b42e65ae9727a1810d6b5a7fb64065
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2026-01-19 21:53:33 +0530

    Refactor flash cmd into function, edit usage/flags

--- main.go
+++ main.go
@@ -18,6 +18,14 @@ const version = "1.0.0-dev"
 var vFlag = flag.Bool("v", false, "")
 var versionFlag = flag.Bool("version", false, "Show version")
 
+func mainUsage() {
+	println("Usage: glassUSB [command] [options]")
+	println("\nAvailable commands:")
+	println("  flash       Flash a Windows ISO to a specific USB device.")
+	println("\nOptions:")
+	flag.PrintDefaults()
+}
+
 var flashFlagSet = flag.NewFlagSet("flash", flag.ExitOnError)
 var gptFlag = flashFlagSet.Bool("gpt", false,
 	"EXPERIMENTAL: Use GPT partitioning instead of MBR.\n"+
@@ -33,8 +41,15 @@ var fsFlag = flashFlagSet.String("fs", "",
 		//"'sources/install.wim' is larger than 4 GB, a second NTFS/exFAT partition will be\n"+
 		//"created to store the WIM file on.\n"+
 		"\nAvailable options: ")
-var disableValidationFlag = flashFlagSet.Bool("disable-validation", false,
-	"Disable validation of written files")
+var skipValidationFlag = flashFlagSet.Bool("skip-validation", false,
+	"Skip validation of written files")
+
+func flashUsage() {
+	println("Usage: glassUSB flash [options] <disk image file> <device path>")
+	println("\nFlash a Windows ISO to a specific USB device.")
+	println("\nOptions:")
+	flashFlagSet.PrintDefaults()
+}
 
 /*
 	 TODO: Support FAT32 + secondary-fs or some kind of splitting
@@ -47,18 +62,8 @@ var disableValidationFlag = flashFlagSet.Bool("disable-validation", false,
 var UEFI_NTFS_IMG []byte
 
 func init() {
-	flag.Usage = func() {
-		println("Usage: glassUSB [command] [options]")
-		println("\nAvailable commands:")
-		println("  flash       Flash a Windows ISO to a specific USB device.")
-		println("\nOptions:")
-		flag.PrintDefaults()
-	}
-	flashFlagSet.Usage = func() {
-		println("Usage: glassUSB flash [options] <disk image file> <device path>")
-		println("\nOptions:")
-		flashFlagSet.PrintDefaults()
-	}
+	flag.Usage = mainUsage
+	flashFlagSet.Usage = flashUsage
 }
 
 func main() {
@@ -67,228 +72,230 @@ func main() {
 		println("glassUSB version v" + version)
 		return
 	} else if len(os.Args) >= 2 && os.Args[1] == "flash" {
-		log.SetFlags(0)
-		log.SetOutput(os.Stderr)
-		log.SetPrefix("[glassUSB] ")
-
-		// Look for prerequisites on system and change defaults accordingly
-		fsFlagStruct := flashFlagSet.Lookup("fs")
-		supportedFilesystems := []string{}
-		if IsNTFSAvailable() {
-			supportedFilesystems = append(supportedFilesystems, "ntfs")
-		}
-		if IsExFATAvailable() {
-			supportedFilesystems = append(supportedFilesystems, "exfat")
-		}
-		if IsFAT32Available() {
-			supportedFilesystems = append(supportedFilesystems, "fat32")
-		}
-		if len(supportedFilesystems) > 0 {
-			fsFlagStruct.DefValue = supportedFilesystems[0]
-			fsFlagStruct.Value.Set(supportedFilesystems[0])
-			fsFlagStruct.Usage = fsFlagStruct.Usage + strings.Join(supportedFilesystems, ", ")
-		}
-		switch supportedFilesystems[0] {
-		case "exfat":
-			log.Println("Warning: NTFS support not found on this system, falling back to exFAT.")
-			log.Println("Warning: exFAT partitions will not boot on PCs with Secure Boot on.")
-		case "fat32":
-			log.Println("Warning: NTFS and exFAT support not found on this system, falling back to FAT32.")
-			log.Println("Warning: Large ISOs (>4GB) will fail to flash to a FAT32 partition.")
-		}
-
-		// Parse flags
-		flashFlagSet.Parse(os.Args[2:])
-		args := flashFlagSet.Args()
-		if len(args) != 2 {
-			flashFlagSet.Usage()
-			os.Exit(1)
-		} else if fsFlag == nil || (*fsFlag != "exfat" && *fsFlag != "ntfs" && *fsFlag != "fat32" && *fsFlag != "") {
-			log.Println("Invalid value provided for `-fs` flag!")
-			flashFlagSet.Usage()
-			os.Exit(1)
-		} else if *fsFlag == "" {
-			log.Fatalln("This system does not have any filesystem drivers supported by glassUSB, exiting...")
-		} else if *fsFlag == "exfat" && !slices.Contains(supportedFilesystems, "exfat") {
-			log.Fatalln("exFAT specified, but support is missing on this system, exiting...")
-		} else if *fsFlag == "ntfs" && !slices.Contains(supportedFilesystems, "ntfs") {
-			log.Fatalln("NTFS specified, but support is missing on this system, exiting...")
-		} else if *fsFlag == "fat32" && !slices.Contains(supportedFilesystems, "fat32") {
-			log.Fatalln("FAT32 specified, but support is missing on this system, exiting...")
-		}
-
-		// Warn about exFAT and FAT32 limitations
-		switch *fsFlag {
-		case "exfat":
-			log.Println("Warning: Drives formatted with exFAT (--fs=exfat) will not boot on PCs with Secure Boot enabled.")
-		case "fat32":
-			log.Println("Warning: Using FAT32 (--fs=fat32) may cause flashing to fail for ISOs larger than 4 GB in size.")
-		}
-
-		totalPhasesNum := 7
-		if *gptFlag {
-			totalPhasesNum-- // Skip MBR writing phase
-		}
-		if *disableValidationFlag {
-			totalPhasesNum-- // Skip validation phase
-		}
-		if *fsFlag == "fat32" {
-			totalPhasesNum-- // Skip UEFI:NTFS writing phase
-		}
-		totalPhases := strconv.Itoa(totalPhasesNum)
-		currentPhase := 0
-
-		// Step 1: Read ISO
-		currentPhase++
-		log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Reading ISO")
-		file, err := os.Open(args[0])
-		if err != nil {
-			log.Fatalf("Failed to open ISO: %v", err)
-		}
-		defer file.Close()
-		srcStat, err := file.Stat()
-		if err != nil {
-			log.Fatalf("Failed to stat ISO file: %v", err)
-		}
-		iso, err := OpenWindowsISO(file)
-		if err != nil {
-			log.Fatalf("Failed to read UDF filesystem on ISO: %v", err)
-		}
-		/* largeFiles := false
-		for _, f := range iso.ReadDir(nil) {
-			if f.Name() == "sources/install.wim" && f.Size() > 4*1024*1024*1024 {
-				largeFiles = true
-			}
-		} */
-
-		// Step 2: Open the block device and create a new partition table
-		blockDevice := args[1]
-		currentPhase++
-		log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Partitioning destination drive")
-		destStat, err := os.Stat(blockDevice)
-		if err != nil {
-			log.Fatalf("Failed to get info about destination: %v", err)
-		} else if destStat.Mode().Type()&os.ModeDevice == 0 {
-			allowRegularDest, exists := os.LookupEnv("__GLASSUSB_DEBUG_ALLOW_REGULAR_DEST")
-			if !exists || (allowRegularDest != "true" && allowRegularDest != "1") {
-				log.Fatalf("Destination %s is not a valid block device!", blockDevice)
-			}
-		}
-		blockDeviceSize, err := GetBlockDeviceSize(blockDevice)
-		if err != nil {
-			log.Fatalf("Failed to get size of destination: %v", err)
-		} else if srcStat.Size() > blockDeviceSize {
-			disableSizeCheck, exists := os.LookupEnv("__GLASSUSB_DEBUG_DISABLE_SIZE_CHECK")
-			if !exists || (disableSizeCheck != "true" && disableSizeCheck != "1") {
-				log.Fatalf("Cannot write ISO to destination: ISO size (%s) is larger than device size (%s)!",
-					imprint.BytesToString(int(srcStat.Size()), true),
-					imprint.BytesToString(int(blockDeviceSize), true))
-			}
-		}
-		err = imprint.UnmountDevice(blockDevice)
-		if err != nil && err != imprint.ErrNotBlockDevice { // Ignore non-block-device error here
-			log.Fatalf("Failed to unmount destination device: %v", err)
-		}
-		if *fsFlag == "fat32" {
-			err = FormatDiskForSinglePartition(blockDevice, gptFlag != nil && *gptFlag)
-		} else {
-			err = FormatDiskForUEFINTFS(blockDevice, gptFlag != nil && *gptFlag)
-		}
-		if err != nil {
-			log.Fatalf("Failed to format disk: %v", err)
-		}
-
-		// Step 3: Write UEFI:NTFS to second partition
-		if *fsFlag != "fat32" {
-			currentPhase++
-			log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Writing UEFI:NTFS bootloader")
-			err = WriteUEFINTFSToPartition(blockDevice, 2)
-			if err != nil {
-				log.Fatalf("Failed to write UEFI bootloader to second partition: %v", err)
-			}
-		}
-
-		// Step 4: Format primary partition depending on fs flag
-		currentPhase++
-		log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Creating sources partition")
-		primaryPartition := GetBlockDevicePartition(blockDevice, 1)
-		switch *fsFlag {
-		case "exfat":
-			if err := MakeExFAT(primaryPartition); err != nil {
-				log.Fatalf("Failed to create exFAT filesystem: %v", err)
-			}
-		case "ntfs":
-			if err := MakeNTFS(primaryPartition); err != nil {
-				log.Fatalf("Failed to create NTFS filesystem: %v", err)
-			}
-		case "fat32":
-			if err := MakeFAT32(primaryPartition); err != nil {
-				log.Fatalf("Failed to create FAT32 filesystem: %v", err)
-			}
-		}
-
-		// Step 5: Unpack Windows ISO contents to primary partition
-		func() {
-			currentPhase++
-			log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Extracting ISO to sources partition")
-			mountPoint, err := os.MkdirTemp(os.TempDir(), "glassusb-")
-			if err != nil {
-				log.Fatalf("Failed to create mount point: %v", err)
-			}
-			defer os.Remove(mountPoint)
-			if err := MountPartition(primaryPartition, mountPoint); err != nil {
-				log.Fatalf("Failed to mount partition: %v", err)
-			}
-			defer func() {
-				if err := UnmountPartition(mountPoint); err != nil {
-					log.Printf("Failed to unmount partition: %v", err)
-				}
-			}()
-			if err := ExtractISOToLocation(iso, mountPoint); err != nil {
-				log.Fatalf("Failed to extract ISO contents: %v", err)
-			}
-		}()
-
-		// Step 6: Validate Windows ISO contents on primary partition
-		func() {
-			if *disableValidationFlag {
-				return
-			}
-			currentPhase++
-			log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Validating ISO contents on sources partition")
-			mountPoint, err := os.MkdirTemp(os.TempDir(), "glassusb-")
-			if err != nil {
-				log.Fatalf("Failed to create mount point: %v", err)
-			}
-			defer os.Remove(mountPoint)
-			if err := MountPartition(primaryPartition, mountPoint); err != nil {
-				log.Fatalf("Failed to mount partition: %v", err)
-			}
-			defer func() {
-				if err := UnmountPartition(mountPoint); err != nil {
-					log.Printf("Failed to unmount partition: %v", err)
-				}
-			}()
-			if err := ValidateISOAgainstLocation(iso, mountPoint); err != nil {
-				log.Fatalf("Failed to validate ISO contents: %v", err)
-			}
-		}()
-
-		// Step 7: Write MBR to device for boot using `ms-sys`
-		if gptFlag == nil || !*gptFlag {
-			currentPhase++
-			log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Writing MBR bootloader")
-			if err := WriteMBRToPartition(primaryPartition); err != nil {
-				log.Fatalf("Failed to write MBR bootloader: %v", err)
-			}
-			if err := WriteMBRToPartition(blockDevice); err != nil {
-				log.Fatalf("Failed to write MBR bootloader: %v", err)
-			}
-		}
-
-		return
+		flashCommand()
 	} else {
 		flag.Usage()
 		os.Exit(1)
+	}
+}
+
+func flashCommand() {
+	log.SetFlags(0)
+	log.SetOutput(os.Stderr)
+	log.SetPrefix("[glassUSB] ")
+
+	// Look for prerequisites on system and change defaults accordingly
+	fsFlagStruct := flashFlagSet.Lookup("fs")
+	supportedFilesystems := []string{}
+	if IsNTFSAvailable() {
+		supportedFilesystems = append(supportedFilesystems, "ntfs")
+	}
+	if IsExFATAvailable() {
+		supportedFilesystems = append(supportedFilesystems, "exfat")
+	}
+	if IsFAT32Available() {
+		supportedFilesystems = append(supportedFilesystems, "fat32")
+	}
+	if len(supportedFilesystems) > 0 {
+		fsFlagStruct.DefValue = supportedFilesystems[0]
+		fsFlagStruct.Value.Set(supportedFilesystems[0])
+		fsFlagStruct.Usage = fsFlagStruct.Usage + strings.Join(supportedFilesystems, ", ")
+	}
+	switch supportedFilesystems[0] {
+	case "exfat":
+		log.Println("Warning: NTFS support not found on this system, falling back to exFAT.")
+		log.Println("Warning: Disks using exFAT will not boot on PCs with Secure Boot on.")
+	case "fat32":
+		log.Println("Warning: NTFS and exFAT support not found on this system, falling back to FAT32.")
+		log.Println("Warning: Large ISOs (>4GB) will fail to flash to a FAT32 partition.")
+	}
+
+	// Parse flags
+	flashFlagSet.Parse(os.Args[2:])
+	args := flashFlagSet.Args()
+	if len(args) != 2 {
+		flashFlagSet.Usage()
+		os.Exit(1)
+	} else if fsFlag == nil || (*fsFlag != "exfat" && *fsFlag != "ntfs" && *fsFlag != "fat32" && *fsFlag != "") {
+		log.Println("Invalid value provided for `-fs` flag!")
+		flashFlagSet.Usage()
+		os.Exit(1)
+	} else if *fsFlag == "" {
+		log.Fatalln("This system does not have any filesystem drivers supported by glassUSB, exiting...")
+	} else if *fsFlag == "exfat" && !slices.Contains(supportedFilesystems, "exfat") {
+		log.Fatalln("exFAT specified, but support is missing on this system, exiting...")
+	} else if *fsFlag == "ntfs" && !slices.Contains(supportedFilesystems, "ntfs") {
+		log.Fatalln("NTFS specified, but support is missing on this system, exiting...")
+	} else if *fsFlag == "fat32" && !slices.Contains(supportedFilesystems, "fat32") {
+		log.Fatalln("FAT32 specified, but support is missing on this system, exiting...")
+	}
+
+	// Warn about exFAT and FAT32 limitations
+	switch *fsFlag {
+	case "exfat":
+		log.Println("Warning: Drives formatted with exFAT (--fs=exfat) will not boot on PCs with Secure Boot enabled.")
+	case "fat32":
+		log.Println("Warning: Using FAT32 (--fs=fat32) may cause flashing to fail for ISOs larger than 4 GB in size.")
+	}
+
+	totalPhasesNum := 7
+	if *gptFlag {
+		totalPhasesNum-- // Skip MBR writing phase
+	}
+	if *skipValidationFlag {
+		totalPhasesNum-- // Skip validation phase
+	}
+	if *fsFlag == "fat32" {
+		totalPhasesNum-- // Skip UEFI:NTFS writing phase
+	}
+	totalPhases := strconv.Itoa(totalPhasesNum)
+	currentPhase := 0
+
+	// Step 1: Read ISO
+	currentPhase++
+	log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Reading ISO")
+	file, err := os.Open(args[0])
+	if err != nil {
+		log.Fatalf("Failed to open ISO: %v", err)
+	}
+	defer file.Close()
+	srcStat, err := file.Stat()
+	if err != nil {
+		log.Fatalf("Failed to stat ISO file: %v", err)
+	}
+	iso, err := OpenWindowsISO(file)
+	if err != nil {
+		log.Fatalf("Failed to read UDF filesystem on ISO: %v", err)
+	}
+	/* largeFiles := false
+	for _, f := range iso.ReadDir(nil) {
+		if f.Name() == "sources/install.wim" && f.Size() > 4*1024*1024*1024 {
+			largeFiles = true
+		}
+	} */
+
+	// Step 2: Open the block device and create a new partition table
+	blockDevice := args[1]
+	currentPhase++
+	log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Partitioning destination drive")
+	destStat, err := os.Stat(blockDevice)
+	if err != nil {
+		log.Fatalf("Failed to get info about destination: %v", err)
+	} else if destStat.Mode().Type()&os.ModeDevice == 0 {
+		allowRegularDest, exists := os.LookupEnv("__GLASSUSB_DEBUG_ALLOW_REGULAR_DEST")
+		if !exists || (allowRegularDest != "true" && allowRegularDest != "1") {
+			log.Fatalf("Destination %s is not a valid block device!", blockDevice)
+		}
+	}
+	blockDeviceSize, err := GetBlockDeviceSize(blockDevice)
+	if err != nil {
+		log.Fatalf("Failed to get size of destination: %v", err)
+	} else if srcStat.Size() > blockDeviceSize {
+		disableSizeCheck, exists := os.LookupEnv("__GLASSUSB_DEBUG_DISABLE_SIZE_CHECK")
+		if !exists || (disableSizeCheck != "true" && disableSizeCheck != "1") {
+			log.Fatalf("Cannot write ISO to destination: ISO size (%s) is larger than device size (%s)!",
+				imprint.BytesToString(int(srcStat.Size()), true),
+				imprint.BytesToString(int(blockDeviceSize), true))
+		}
+	}
+	err = imprint.UnmountDevice(blockDevice)
+	if err != nil && err != imprint.ErrNotBlockDevice { // Ignore non-block-device error here
+		log.Fatalf("Failed to unmount destination device: %v", err)
+	}
+	if *fsFlag == "fat32" {
+		err = FormatDiskForSinglePartition(blockDevice, gptFlag != nil && *gptFlag)
+	} else {
+		err = FormatDiskForUEFINTFS(blockDevice, gptFlag != nil && *gptFlag)
+	}
+	if err != nil {
+		log.Fatalf("Failed to format disk: %v", err)
+	}
+
+	// Step 3: Write UEFI:NTFS to second partition
+	if *fsFlag != "fat32" {
+		currentPhase++
+		log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Writing UEFI:NTFS bootloader")
+		err = WriteUEFINTFSToPartition(blockDevice, 2)
+		if err != nil {
+			log.Fatalf("Failed to write UEFI bootloader to second partition: %v", err)
+		}
+	}
+
+	// Step 4: Format primary partition depending on fs flag
+	currentPhase++
+	log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Creating sources partition")
+	primaryPartition := GetBlockDevicePartition(blockDevice, 1)
+	switch *fsFlag {
+	case "exfat":
+		if err := MakeExFAT(primaryPartition); err != nil {
+			log.Fatalf("Failed to create exFAT filesystem: %v", err)
+		}
+	case "ntfs":
+		if err := MakeNTFS(primaryPartition); err != nil {
+			log.Fatalf("Failed to create NTFS filesystem: %v", err)
+		}
+	case "fat32":
+		if err := MakeFAT32(primaryPartition); err != nil {
+			log.Fatalf("Failed to create FAT32 filesystem: %v", err)
+		}
+	}
+
+	// Step 5: Unpack Windows ISO contents to primary partition
+	func() {
+		currentPhase++
+		log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Extracting ISO to sources partition")
+		mountPoint, err := os.MkdirTemp(os.TempDir(), "glassusb-")
+		if err != nil {
+			log.Fatalf("Failed to create mount point: %v", err)
+		}
+		defer os.Remove(mountPoint)
+		if err := MountPartition(primaryPartition, mountPoint); err != nil {
+			log.Fatalf("Failed to mount partition: %v", err)
+		}
+		defer func() {
+			if err := UnmountPartition(mountPoint); err != nil {
+				log.Printf("Failed to unmount partition: %v", err)
+			}
+		}()
+		if err := ExtractISOToLocation(iso, mountPoint); err != nil {
+			log.Fatalf("Failed to extract ISO contents: %v", err)
+		}
+	}()
+
+	// Step 6: Validate Windows ISO contents on primary partition
+	func() {
+		if *skipValidationFlag {
+			return
+		}
+		currentPhase++
+		log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Validating ISO contents on sources partition")
+		mountPoint, err := os.MkdirTemp(os.TempDir(), "glassusb-")
+		if err != nil {
+			log.Fatalf("Failed to create mount point: %v", err)
+		}
+		defer os.Remove(mountPoint)
+		if err := MountPartition(primaryPartition, mountPoint); err != nil {
+			log.Fatalf("Failed to mount partition: %v", err)
+		}
+		defer func() {
+			if err := UnmountPartition(mountPoint); err != nil {
+				log.Printf("Failed to unmount partition: %v", err)
+			}
+		}()
+		if err := ValidateISOAgainstLocation(iso, mountPoint); err != nil {
+			log.Fatalf("Failed to validate ISO contents: %v", err)
+		}
+	}()
+
+	// Step 7: Write MBR to device for boot using `ms-sys`
+	if gptFlag == nil || !*gptFlag {
+		currentPhase++
+		log.Println("Phase " + strconv.Itoa(currentPhase) + "/" + totalPhases + ": Writing MBR bootloader")
+		if err := WriteMBRToPartition(primaryPartition); err != nil {
+			log.Fatalf("Failed to write MBR bootloader: %v", err)
+		}
+		if err := WriteMBRToPartition(blockDevice); err != nil {
+			log.Fatalf("Failed to write MBR bootloader: %v", err)
+		}
 	}
 }
