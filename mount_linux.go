a2a54306fa549541249b3b8ae4c8ca7bcb2a1dbb
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2026-04-19 19:45:59 +0530

    Use syscall.Unmount on Linux

--- mount_linux.go
+++ mount_linux.go
@@ -5,10 +5,12 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"syscall"
 )
 
 func MountPartition(partition string, mountPoint string) error {
 	// TODO: use syscall.Mount instead?
+	// if err := syscall.Mount(partition, mountPoint, "", 0, ""); err != nil {
 	if out, err := exec.Command("mount", partition, mountPoint).CombinedOutput(); err != nil {
 		return fmt.Errorf("failed to mount partition: %w\noutput: %s", err, out)
 	}
@@ -16,9 +18,8 @@ func MountPartition(partition string, mountPoint string) error {
 }
 
 func UnmountPartition(mountPoint string) error {
-	// TODO: use syscall.Unmount instead?
-	if out, err := exec.Command("umount", mountPoint).CombinedOutput(); err != nil {
-		return fmt.Errorf("failed to unmount partition: %w\noutput: %s", err, out)
+	if err := syscall.Unmount(mountPoint, 0); err != nil {
+		return fmt.Errorf("failed to unmount partition: %w", err)
 	}
 	return nil
 }
