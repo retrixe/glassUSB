75433326e7cacf24fcf32a398d7bd67a28c23a10
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2026-08-16 20:28:20 +0530

    WIP: Move to a TUI using bubbletea
    Issues so far:
    - The filepicker is finnicky and not great.
    - Devices aren't loaded so far, and no reload option yet.
    - TUI has not been integrated into the rest of the code.
    
    At this point, I would like to trash any wizard code inside glassUSB and
    utilise the ready-made Imprint codebase instead. I thought this could
    be faster, but Zenity having issues while running as root means this
    entire exercise is futile.

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 
 	_ "embed"
 
+	tea "charm.land/bubbletea/v2"
 	"github.com/ncruces/zenity"
 	"github.com/retrixe/imprint/imaging"
 )
@@ -99,9 +100,12 @@ func main() {
 		}
 	} else if len(os.Args) >= 2 && os.Args[1] == "wizard" {
 		flashFlagSet.Usage = flashWizardUsage
-		if err := flashCommand(true); err != nil {
+		p := tea.NewProgram(initialModel())
+		_, err := p.Run()
+		if err != nil {
 			log.Fatalln(err)
 		}
+		// flashCommand(true) // FIXME: Pass parameters from model and trash the zenity code
 	} else {
 		flag.Usage()
 		os.Exit(1)
